b44e70047675abcf54725d7fa225f91b409c21db
arjunpkulkarni <arjunpkulkarni@gmail.com>
2024-12-29 16:36:37 -0500

    new push

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,46 @@
 "use client";
 
+import { Home, FolderClosed, Settings, Smartphone } from "lucide-react";
+
 export default function Nav() {
   return (
-    <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-8 py-4 flex justify-between items-center">
-        <a href="/" className="text-gray-800 font-medium hover:text-purple-500 transition duration-300">
-          Home
-        </a>
-        <a href="/files" className="text-gray-800 font-medium hover:text-purple-500 transition duration-300">
-          Files
-        </a>
-        <a href="/og" className="text-gray-800 font-medium hover:text-purple-500 transition duration-300">
-          OG
-        </a>
-        <a href="/settings" className="text-gray-800 font-medium hover:text-purple-500 transition duration-300">
-          Settings
-        </a>
-        <a href="/mobile" className="text-gray-800 font-medium hover:text-purple-500 transition duration-300">
-          Mobile
-        </a>
+    <nav className="flex gap-8 items-center bg-black justify-center">
+      <div className="mt-4 flex flex-row gap-6">
+      <a
+        href="/"
+        className="text-white opacity-70 hover:opacity-100 transition-opacity duration-300 flex items-center"
+        title="Home"
+      >
+        <Home className="w-6 h-6" />
+      </a>
+      <a
+        href="/files"
+        className="text-white opacity-70 hover:opacity-100 transition-opacity duration-300 flex items-center"
+        title="Files"
+      >
+        <FolderClosed className="w-6 h-6" />
+      </a>
+      <a
+        href="/og"
+        className="text-white opacity-70 hover:opacity-100 transition-opacity duration-300 flex items-center"
+        title="OG"
+      >
+        OG
+      </a>
+      <a
+        href="/settings"
+        className="text-white opacity-70 hover:opacity-100 transition-opacity duration-300 flex items-center"
+        title="Settings"
+      >
+        <Settings className="w-6 h-6" />
+      </a>
+      <a
+        href="/mobile"
+        className="text-white opacity-70 hover:opacity-100 transition-opacity duration-300 flex items-center"
+        title="Mobile"
+      >
+        <Smartphone className="w-6 h-6" />
+      </a>
       </div>
     </nav>
   );
